713ad9003db09a8988488c011b4682aa753b1132
佐藤真 <ac.backfill1539@gmail.com>
2018-11-14 23:03:44 +0900

    カスタムインスペクターにデフォルトインスペクターを表示，メニューバーに機能追加

--- Assets/Editor/LookAtPointEditor.cs
+++ Assets/Editor/LookAtPointEditor.cs
@@ -3,19 +3,36 @@ using UnityEditor;
 
 [CustomEditor(typeof(LookAtPoint))]
 [CanEditMultipleObjects]
-public class LookAtPointEditor : Editor
-{
+public class LookAtPointEditor : Editor {
     SerializedProperty lookAtPoint;
 
-    void OnEnable()
-    {
+    void OnEnable(){
         lookAtPoint = serializedObject.FindProperty("lookAtPoint");
     }
 
-    public override void OnInspectorGUI()
-    {
+    public override void OnInspectorGUI(){
         serializedObject.Update();
         EditorGUILayout.PropertyField(lookAtPoint);
         serializedObject.ApplyModifiedProperties();
+        if (lookAtPoint.vector3Value.y>(target as LookAtPoint).transform.position.y){
+            EditorGUILayout.LabelField("(Above this object)");
+        }
+        if (lookAtPoint.vector3Value.y<(target as LookAtPoint).transform.position.y){
+            EditorGUILayout.LabelField("(Below this object)");
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    public void OnSceneGUI() {
+        var t = (target as LookAtPoint);
+
+        EditorGUI.BeginChangeCheck();
+        Vector3 pos = Handles.PositionHandle(t.lookAtPoint, Quaternion.identity);
+        if (EditorGUI.EndChangeCheck()){
+            Undo.RecordObject(target, "Move point");
+            t.lookAtPoint = pos;
+            //t.Update();
+        }
     }
 }
