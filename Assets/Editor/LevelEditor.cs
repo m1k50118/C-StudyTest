05fd52ae8d8303abfd23565b4111fed89756b7a9
佐藤真 <ac.backfill1539@gmail.com>
2018-11-14 18:50:23 +0900

    First Commit

---
+++ Assets/Editor/LevelEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class LevelEditor : Editor {
+
+	[CustomEditor(typeof(Level))]
+	[CanEditMultipleObjects]
+	public override void OnInspectorGUI(){
+        serializedObject.Update();
+		Level myLevel = (Level)target;
+		myLevel.experience = EditorGUILayout.IntField("Experience", myLevel.experience);
+        EditorGUILayout.LabelField("Level", myLevel.level.ToString());
+	}
+}
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    public int experience;
+
+	public int level {
+		get {
+			return experience/750;
+		}
+	}
+}
